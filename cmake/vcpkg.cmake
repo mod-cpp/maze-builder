601065e46338c3a194255913042e862a78e60d6c
Patricia Aas <patricia@turtlesec.no>
2022-05-20 15:24:40 +0200

    Update project

--- cmake/vcpkg.cmake
+++ cmake/vcpkg.cmake
@@ -2,17 +2,17 @@ include(FetchContent)
 FetchContent_Declare(
         vcpkg
         GIT_REPOSITORY https://github.com/microsoft/vcpkg
-        GIT_TAG 8e4cfac84bf6fbb081123b02f8c65bad8418b25e
+        GIT_TAG af2287382b1991dbdcb7e5112d236f3323b9dd7a # 2022.03.10
 )
 
-if (NOT DEFINED CMAKE_TOOLCHAIN_FILE)
+if(NOT DEFINED CMAKE_TOOLCHAIN_FILE)
     FetchContent_GetProperties(vcpkg
-                               POPULATED vcpkg_POPULATED
-                               SOURCE_DIR vcpkg_SOURCE_DIR)
-    if (NOT vcpkg_POPULATED)
+            POPULATED vcpkg_POPULATED
+            SOURCE_DIR vcpkg_SOURCE_DIR)
+    if(NOT vcpkg_POPULATED)
         FetchContent_Populate(vcpkg)
-    endif ()
-endif ()
+    endif()
+endif()
 
 set(CMAKE_TOOLCHAIN_FILE "${vcpkg_SOURCE_DIR}/scripts/buildsystems/vcpkg.cmake")
 set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${CMAKE_CURRENT_LIST_DIR}/toolchain.cmake")
